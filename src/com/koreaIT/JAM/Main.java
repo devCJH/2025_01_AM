8e83c7ee9ef819a40d11b4ec371f12dadf840aa4
devCJH <http-@naver.com>
2025-02-05 16:00:47 +0900

    article write, article list

--- src/com/koreaIT/JAM/Main.java
+++ src/com/koreaIT/JAM/Main.java
@@ -1,5 +1,7 @@
 package com.koreaIT.JAM;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Main {
@@ -8,6 +10,10 @@ class Main {
 		
 		Scanner sc = new Scanner(System.in);
 		
+		int articleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
+		
 		while (true) {
 			System.out.printf("명령어) ");
 			String cmd = sc.nextLine();
@@ -15,10 +21,49 @@ class Main {
 			if (cmd.equals("exit")) {
 				break;
 			}
+			
+			if (cmd.equals("article list")) {
+				if (articles.size() == 0) {
+					System.out.println("게시물이 존재하지 않습니다");
+					continue;
+				}
+				
+				System.out.println("번호	|	제목");
+				
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+			}
+			
+			if (cmd.equals("article write")) {
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				articleId++;
+				
+				Article article = new Article(articleId, title, body);
+				articles.add(article);
+				
+				System.out.printf("%d번 글이 생성되었습니다\n", articleId);
+			}
 		}
 		
 		sc.close();
 		
 		System.out.println("== 프로그램 끝 ==");
+	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
